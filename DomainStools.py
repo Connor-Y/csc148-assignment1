db9849b18d350dd260378d088fc3d2c7a11b49dc
Lawrence Wu <lawrence@lawrencewu.me>
2013-09-25 11:18:43 -0400

    initial problem

---
+++ DomainStools.py
@@ -0,0 +1,47 @@
+# Copyright 2013 Gary Baumgartner
+# Distributed under the terms of the GNU General Public License.
+#
+# This file is part of Assignment 1, CSC148, Fall 2013.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <http://www.gnu.org/licenses/>.
+"""
+DomainStools:  Model Anne Hoy's stools holding cheeses.
+Cheese:   Model a cheese with a given size
+"""
+
+
+class DomainStools:
+    """Model Anne Hoy's stools holding cheese.
+
+    Model stools holding stacks of cheese, enforcing the constraint
+    that a larger cheese may not be placed on a smaller one.
+    """
+
+    # TODO: See Steps 2 and 3 of the handout.
+    pass
+
+
+class Cheese:
+    def __init__(self: 'Cheese', size: float) -> None:
+        """
+        Initialize a Cheese to diameter size.
+
+        >>> c = Cheese(3)
+        >>> isinstance(c, Cheese)
+        True
+        >>> c.size
+        3
+        """
+
+        self.size = size
